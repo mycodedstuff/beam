6c130718043673f9c453d97c211d6f811177e33e
Kirill Boltaev <aske@fmap.me>
2020-10-06 01:59:23 +0300

    w

--- flake.nix
+++ flake.nix
@@ -5,46 +5,10 @@
   inputs.euler-build.url = flake:euler-build/wip;
   inputs.euler-build.inputs.flake-utils.follows = "flake-utils";
 
-  # outputs = { self, euler-build, flake-utils }:
-  #   {
-  #     overlay = import ./nix/overlay.nix;
-  #   } //
-  #   (flake-utils.lib.eachDefaultSystem (system:
-  #     let
-  #       pkgs = import euler-build.nixpkgs {
-  #         inherit system;
-  #         overlays = [ euler-build.overlay self.overlay ];
-  #       };
-  #     in
-  #       {
-  #         packages = with pkgs.eulerHaskellPackages; {
-  #          inherit beam-core;
-  #          inherit beam-migrate;
-  #          inherit beam-migrate-cli;
-  #          inherit beam-postgres;
-  #          inherit beam-sqlite;
-  #          inherit interpolate;
-  #         };
-
-  #         defaultPackage = pkgs.eulerHaskellPackages.beam-core;
-  #       }
-  #   ));
-
   outputs = flakeInputs@{ self, ... }:
     flakeInputs.euler-build.mkEulerFlake {
-      flakeName = "beam";
-      defaultPackageName = "beam-core";
-      exportPackages = [
-        "beam-core"
-        "beam-migrate"
-        "beam-migrate-cli"
-        "beam-postgres"
-        "beam-sqlite"
-      ];
-      overlayPath = "./nix/overlay.nix";
-      shellToolsPath = ./shell-tools.nix;
-      # configPath = ./confix.nix; # TODO: in euler-build
-      # if exists then ... else generate default exports
+      overlayPath = ./nix/overlay.nix;
+      configPath = ./nix/confix.nix;
       inputs = flakeInputs;
     };
 }
