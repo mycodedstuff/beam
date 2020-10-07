f17140f14ffbbf0786f533023169faea6fcec114
Kirill Boltaev <aske@fmap.me>
2020-10-07 12:22:23 +0300

    wip

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.euler-build.url = flake:euler-build/wip;
-  inputs.euler-build.inputs.flake-utils.follows = "flake-utils";
+  # inputs.euler-build.inputs.flake-utils.follows = "flake-utils";
 
   outputs = flakeInputs@{ self, ... }:
     flakeInputs.euler-build.mkEulerFlake {
       overlayPath = ./nix/overlay.nix;
-      configPath = ./nix/confix.nix;
+      configPath = ./nix/config.nix;
       inputs = flakeInputs;
     };
 }
